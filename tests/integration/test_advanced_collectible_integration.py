93dba78067910d5de255a87565c01fda261efa29
Atiqur Rahman <atiqur.india@gmail.com>
2022-03-31 19:07:33 +0530

    Create integration test

---
+++ tests/integration/test_advanced_collectible_integration.py
@@ -0,0 +1,23 @@
+import time
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    get_contract,
+)
+from scripts.advanced_collectible.deploy_and_create import deploy_and_create
+from brownie import network, AdvancedCollectible
+import pytest
+
+
+def test_can_create_advanced_collectible():
+    # deploy the contract
+    # create an NFT
+    # get a random breed back
+    # Arrange
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for integration testing")
+    # Act
+    advanced_collectible, creation_transaction = deploy_and_create()
+    time.sleep(60)
+    # Assert
+    assert advanced_collectible.tokenCounter() == 1
